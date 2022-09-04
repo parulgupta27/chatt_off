241c85bda8323e39b001e91e1fd8eb7c3fea4d04
parulgupta27 <112709768+parulgupta27@users.noreply.github.com>
2022-09-04 15:33:19 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:chat_off/screens/welcome.dart';
+import 'package:chat_off/screens/login_screen.dart';
+import 'package:chat_off/screens/registration_screen.dart';
+import 'package:chat_off/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+} 
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: '/',
+      routes: 
+      {
+        '/':(context) => WelcomeScreen(),
+        '/login':(context) => LoginScreen(),
+        '/register':(context) => RegistrationScreen(),
+        '/chat':(context) => ChatScreen(),
+      },
+    );
+  }
+}
+
